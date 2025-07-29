5a35768ad09e4fdca61fcfea5675ab423fc22fe3
Aleksa Djurdjevic <aleksadjurdjevic015@gmail.com>
2025-07-29 22:11:26 +0200

    Started implementing individual descriptors and their handling mechanism

---
+++ include/Descriptors/StuffingDescriptor.h
@@ -0,0 +1,38 @@
+#ifndef _STUFFING_DESCRIPTOR_H
+#define _STUFFING_DESCRIPTOR_H
+
+#include <memory>
+#include <cstdint>
+#include <cstddef>
+
+#include "Descriptor.h"
+
+/*
+    STD: The stuffing descriptor provides a means of invalidating previously
+    coded descriptors or inserting dummy descriptors for table stuffing.
+*/
+
+namespace Descriptors {
+
+    class StuffingDescriptor: Descriptor {
+
+        std::unique_ptr<std::uint8_t[]> stuffing_bytes;
+
+    public:
+        static constexpr std::uint8_t tag = 0x42;
+        
+        StuffingDescriptor(std::size_t len, const std::uint8_t* start)
+            : Descriptor(len), stuffing_bytes(std::make_unique<std::uint8_t[]>(len)) {
+                
+            std::copy(start, start + len, this->stuffing_bytes.get());
+        }
+
+        StuffingDescriptor(StuffingDescriptor&&) = default;
+        StuffingDescriptor& operator=(StuffingDescriptor&&) = default;
+        ~StuffingDescriptor() = default;
+
+        void print(std::size_t) const override;
+    };
+}
+
+#endif // _STUFFING_DESCRIPTOR_H
--- src/Utils.cpp
+++ src/Utils.cpp
@@ -1,9 +1,86 @@
 #include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <cstdint>
+#include <string>
+#include <cstring>
+#include <optional>
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
 
 #include "Utils.h"
 
-void utils::printLine(const std::string& label, int indent, char border) {
+void utils::printLine(const std::string& label, size_t indent, char border) {
     std::cout << std::string(indent * 4, ' ')
                 << std::string(5, border) << " " << label << " "
                 << std::string(5, border) << std::endl;
 }
+
+void utils::printByteArrayHex(const std::string& label, const std::uint8_t* array, std::size_t indent_level) {
+
+    std::cout << std::string(indent_level * 4, ' ') << 
+
+} // NASTAVI PRINT STUFFING DESCRIPTORA
+
+
+// utils::mmap::mmap_t::mmap_t(const char* input_file) {
+
+//     // Memory map and construct the object
+//     int32_t ts_file_fd = ::open(input_file_name, O_RDONLY);
+//     if (ts_file_fd < 0) {
+//         std::cerr << "[TS] Failed to open the ts file." << std::endl;
+//         this->data = nullptr;
+//         this->length = 0;
+//         return;
+//     }
+
+//     struct stat ts_file_stat;
+//     if (fstat(static_cast<int>(ts_file_fd), &ts_file_stat) < 0) {
+//         std::cerr << "[TS] Failed to get ts file size." << std::endl;
+//         ::close(ts_file_fd);
+//         this->data = nullptr;
+//         this->length = 0;
+//         return;
+//     }
+
+//     size_t ts_file_size = static_cast<size_t>(ts_file_stat.st_size);
+
+//     void* mapped_ts = mmap(nullptr, ts_file_size, PROT_READ, MAP_PRIVATE, ts_file_fd, 0);
+//     if (mapped_ts == MAP_FAILED) {
+//         std::cerr << "[TS] Failed to memory map the ts file into process memory." << std::endl;
+//         ::close(ts_file_fd);
+//         return std::nullopt;
+//     }
+//     ::close(ts_file_fd);
+
+// }
+
+// std::optional<utils::mmap::mmap_t> utils::mmap::mmap_posix(const char* input_file_name) {
+//     int32_t ts_file_fd = ::open(input_file_name, O_RDONLY);
+//     if (ts_file_fd < 0) {
+//         std::cerr << "[TS] Failed to open the ts file." << std::endl;
+//         return std::nullopt;
+//     }
+
+//     struct stat ts_file_stat;
+//     if (fstat(static_cast<int>(ts_file_fd), &ts_file_stat) < 0) {
+//         std::cerr << "[TS] Failed to get ts file size." << std::endl;
+//         ::close(ts_file_fd);
+//         return std::nullopt;
+//     }
+
+//     size_t ts_file_size = static_cast<size_t>(ts_file_stat.st_size);
+
+//     void* mapped_ts = mmap(nullptr, ts_file_size, PROT_READ, MAP_PRIVATE, ts_file_fd, 0);
+//     if (mapped_ts == MAP_FAILED) {
+//         std::cerr << "[TS] Failed to memory map the ts file into process memory." << std::endl;
+//         ::close(ts_file_fd);
+//         return std::nullopt;
+//     }
+//     ::close(ts_file_fd);
+
+//     return map<const uint8_t*>(mapped_ts, ts_file_size);
+// }
