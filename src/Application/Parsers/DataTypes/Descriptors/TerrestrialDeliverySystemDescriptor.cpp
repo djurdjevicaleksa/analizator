7c45c6db603b5ecd0a90cccedae9f15cc40a39ce
Aleksa Djurdjevic <aleksadjurdjevic015@gmail.com>
2025-07-31 20:15:49 +0200

    Started the Bazel update

---
+++ src/Application/Parsers/DataTypes/Descriptors/TerrestrialDeliverySystemDescriptor.cpp
@@ -0,0 +1,113 @@
+#include <cstddef>
+#include <cstdint>
+#include <stdexcept>
+
+#include "TerrestrialDeliverySystemDescriptor.h"
+#include "Utils.h"
+
+Descriptors::TerrestrialDeliverySystemDescriptor::TerrestrialDeliverySystemDescriptor(std::size_t len, const std::uint8_t* start)
+    : Descriptor(len) {
+
+    this->centre_frequency = utils::readMSB32(start);
+    this->bandwidth = (start[4] >> 5) & 0b111;
+    this->priority = (start[4] >> 4) & 0b1;
+    this->time_slicing_indicator = (start[4] >> 3) & 0b1;
+    this->mpe_fec_indicator = (start[4] >> 2) & 0b1;
+    this->constellation = (start[5] >> 6) & 0b11;
+    this->hierarchy_information = (start[5] >> 3) & 0b111;
+    this->code_rate_hp_stream = start[5] & 0b111;
+    this->code_rate_lp_stream = (start[6] >> 5) & 0b111;
+    this->guard_interval = (start[6] >> 3) & 0b11;
+    this->transmission_mode = (start[6] >> 1) & 0b11;
+    this->other_frequency_flag = start[6] & 0b1;
+}
+
+std::string Descriptors::TerrestrialDeliverySystemDescriptor::deduceBandwidth() const {
+    switch(this->bandwidth) {
+        case 0: return "8 MHz";
+        case 1: return "7 MHz";
+        case 2: return "6 MHz";
+        case 3: return "5 MHz";
+        default: return "Reserved for future use";
+    }
+}
+
+std::string Descriptors::TerrestrialDeliverySystemDescriptor::deduceConstellationPattern() const {
+    switch(this->constellation) {
+        case 0: return "QPSK";
+        case 1: return "16-QAM";
+        case 2: return "64-QAM";
+        default: return "Reserved for future use";
+    }
+}
+
+std::string Descriptors::TerrestrialDeliverySystemDescriptor::deduceHierarchyInformation() const {
+    switch(this->hierarchy_information) {
+        case 0: return "Non-hierarchical, native interleaver";
+        case 1: return "Alpha=1, native interleaver";
+        case 2: return "Alpha=2, native interleaver";
+        case 3: return "Alpha=4, native interleaver";
+        case 4: return "Non-hierarchical, in-depth interleaver";
+        case 5: return "Alpha=1, in-depth interleaver";
+        case 6: return "Alpha=2, in-depth interleaver";
+        case 7: return "Alpha=4, in-depth interleaver";
+        default: throw std::runtime_error("[TerrestrialDeliverySystemDescriptor] Failed to deduce the hierarchy information: Invalid hierarchy information bitfield value.");
+    }
+}
+
+std::string Descriptors::TerrestrialDeliverySystemDescriptor::deduceCodeRate() const {
+    if (this->hierarchy_information == 0 || this->hierarchy_information == 4) {
+        // Non-hierarchical
+        switch(this->code_rate_hp_stream) {
+            case 0: return "Non-hierarchical, 1/2 modulation";
+            case 1: return "Non-hierarchical, 2/3 modulation";
+            case 2: return "Non-hierarchical, 3/4 modulation";
+            case 3: return "Non-hierarchical, 5/6 modulation";
+            case 4: return "Non-hierarchical, 7/8 modulation";
+            default: return "Reserved for future use";
+        }
+    }
+    else {
+        // Hierarchical
+        std::string ret = "Hierarchical, ";
+        switch(this->code_rate_hp_stream) {
+            case 0: ret += "High-priority: 1/2 modulation"; break;
+            case 1: ret += "High-priority: 2/3 modulation"; break;
+            case 2: ret += "High-priority: 3/4 modulation"; break;
+            case 3: ret += "High-priority: 5/6 modulation"; break;
+            case 4: ret += "High-priority: 7/8 modulation"; break;
+            default: ret += "High-priority: Reserved for future use"; break;
+        }
+
+        ret += "  ";
+
+        switch(this->code_rate_lp_stream) {
+            case 0: ret += "Low-priority: 1/2 modulation"; break;
+            case 1: ret += "Low-priority: 2/3 modulation"; break;
+            case 2: ret += "Low-priority: 3/4 modulation"; break;
+            case 3: ret += "Low-priority: 5/6 modulation"; break;
+            case 4: ret += "Low-priority: 7/8 modulation"; break;
+            default: ret += "Low-priority: Reserved for future use"; break;
+        }
+
+        return ret;
+    }
+}
+
+std::string Descriptors::TerrestrialDeliverySystemDescriptor::deduceGuardInterval() const {
+    switch(this->guard_interval) {
+        case 0: return "1/32";
+        case 1: return "1/16";
+        case 2: return "1/8";
+        case 3: return "1/4";
+    }
+}
+
+std::string Descriptors::TerrestrialDeliverySystemDescriptor::deduceTransmissionMode() const {
+    switch(this->transmission_mode) {
+        case 0: return "2K mode";
+        case 1: return "8K mode";
+        case 2: return "4K mode";
+        case 3: return "Reserved for future use";
+    }
+}
---
+++ src/Application/Parsers/DataTypes/Descriptors/TerrestrialDeliverySystemDescriptor.h
@@ -0,0 +1,69 @@
+#ifndef _TERRESTRIAL_DELIVERY_SYSTEM_DESCRIPTOR_H
+#define _TERRESTRIAL_DELIVERY_SYSTEM_DESCRIPTOR_H
+
+#include <cstdint>
+#include <cstddef>
+#include <string>
+
+#include "Descriptor.h" 
+#include "DescriptorTemplatization.h"
+
+namespace Descriptors {
+
+    class TerrestrialDeliverySystemDescriptor: Descriptor {
+
+        std::uint32_t centre_frequency;
+        std::uint8_t bandwidth;
+        bool priority;
+        bool time_slicing_indicator;
+        bool mpe_fec_indicator;
+        std::uint8_t constellation;
+        std::uint8_t hierarchy_information;
+        std::uint8_t code_rate_hp_stream;
+        std::uint8_t code_rate_lp_stream;
+        std::uint8_t guard_interval;
+        std::uint8_t transmission_mode;
+        bool other_frequency_flag;
+
+    public:
+        static constexpr std::uint8_t tag = 0x5A;
+
+        TerrestrialDeliverySystemDescriptor(std::size_t len, const std::uint8_t* start);
+
+        TerrestrialDeliverySystemDescriptor(TerrestrialDeliverySystemDescriptor&&) = default;
+        TerrestrialDeliverySystemDescriptor& operator=(TerrestrialDeliverySystemDescriptor&&) = default;
+        ~TerrestrialDeliverySystemDescriptor() = default;        
+
+        inline std::uint64_t deduceCentreFrequencyHz() const {
+            return static_cast<std::uint64_t>(this->centre_frequency) * 10;
+        }
+
+        inline float deduceCentreFrequencyKHz() const {
+            return static_cast<float>(this->centre_frequency) / 100.0;
+        }
+
+        inline double deduceCentreFrequencyMHz() const {
+            return static_cast<double>(this->centre_frequency) / 100'000.0;
+        }
+
+        inline double deduceCentreFrequencyGHz() const {
+            return static_cast<double>(this->centre_frequency) / 100'000'000.0;
+        }
+
+        std::string deduceBandwidth() const;
+
+        inline std::string deducePriority() const {
+            return this->priority == 0? "High priority" : "Low priority";
+        }
+
+        std::string deduceConstellationPattern() const;
+        std::string deduceHierarchyInformation() const;
+        std::string deduceCodeRate() const;
+        std::string deduceGuardInterval() const;
+        std::string deduceTransmissionMode() const;
+    };
+}
+
+template<> struct DerivedDescriptorFromTag<Descriptors::TerrestrialDeliverySystemDescriptor::tag> { using type = Descriptors::TerrestrialDeliverySystemDescriptor; };
+
+#endif // _TERRESTRIAL_DELIVERY_SYSTEM_DESCRIPTOR_H
